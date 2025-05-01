4ad2a986a002164472f7848d44b7106767b5f163
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-04-30 23:03:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
 from flask import Flask
+import requests
+import json
+
+
 app = Flask(__name__)
 
+app.route('/get-subscription-status', methods=['GET'])
+def get_subscription_status():
+  data = request.get_json()
+  person = data.get('person')
 
+  with open("subscription-status.json", 'r') as file:
+    status = json.load(file)
+
+  for user in status:
+      if user["name"].lower() == person.lower():  # case-insensitive match
+          print(f"Found: ID={user['user_id']}, Status={user['status']},")
+          return jsonify({
+                "user_id": user["user_id"],
+                "status": user["status"],
+                "expires": user["expires"]
+            })
+
+          break
+  else:
+      print("User not found.")
