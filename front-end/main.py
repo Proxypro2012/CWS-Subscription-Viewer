3f61f42d68d035fd1f424b0cd55194603dd81663
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-05-01 21:06:29 -0400

    Update main.py

--- front-end/main.py
+++ front-end/main.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+st.set_page_config(
+    layout="wide",
+    page_title="CWS Subscription Viewer",
+    page_icon="front-end/logo.png",  # Optionally, set a page icon
+    initial_sidebar_state="expanded"
+)
+
+
 r1col1, r1col2, r1col3 = st.columns([2, 0.5, 0.5])
 r2col1, r2col2, r2col3 = st.columns([1, 2, 1])
 r3col1, r3col2, r3col3 = st.columns([1, 2, 1])
@@ -9,3 +17,8 @@ r5col1, r5col2, r5col3 = st.columns([1, 2, 1])
 
 with r1col1:
   st.title("Subscription Status")
+
+
+menuOptions = []
+menuOptions.extend(["Subscription Status", "BlankSite"])
+selected_page = st.sidebar.radio("Navigation", options=menuOptions)
