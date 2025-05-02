02cdc5b7209cd771243de4cd325b164c9bd6a3af
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-05-01 21:30:25 -0400

    Update main.py

--- front-end/main.py
+++ front-end/main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import streamlit.components.v1 as components
+import requests
 
 st.set_page_config(
     layout="wide",
@@ -7,6 +9,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+BASE_URL = "https://cws-subscription-viewer.onrender.com/"
 
 r1col1, r1col2, r1col3 = st.columns([2, 0.5, 0.5])
 r2col1, r2col2, r2col3 = st.columns([1, 2, 1])
@@ -31,8 +34,15 @@ selected_page = st.sidebar.radio("", options=menuOptions)
 if selected_page == menuOptions[0]:
     with r1col1:
       st.title("Subscription Status")
+    spawn_status_widgets()
 
 
+def spawn_status_widgets():
+    url = str(f"{BASE_URL}get-subscriber-count")
+    response = requests.get(BASE_URL)
+    for i in int(response):
+        st.status("Website Under Development", key=i-1)
+        
 
 
 
