d4b73edbcae0e807a3b6cedca7079b1030dabdb3
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-05-01 21:11:51 -0400

    Update main.py

--- front-end/main.py
+++ front-end/main.py
@@ -17,7 +17,7 @@ r5col1, r5col2, r5col3 = st.columns([1, 2, 1])
 
 # Sidebar 
 menuOptions = []
-st.sidebar.header("Navigation")
+st.sidebar.title("Navigation")
 st.sidebar.image("front-end/header-logo.svg")
 menuOptions.extend(["Subscription Status", "BlankSite"])
 selected_page = st.sidebar.radio("", options=menuOptions)
