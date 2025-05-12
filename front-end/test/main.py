e23a30be906f1503e88f2f361f7ccb9a865498d4
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-05-11 21:51:00 -0400

    Update main.py

--- front-end/test/main.py
+++ front-end/test/main.py
@@ -1,1 +1,14 @@
+r1col1, r1col2, r1col3 = st.columns([1, 2, 1])
+r2col1, r2col2, r2col3 = st.columns([0.25, 4, 0.25])
+r3col1, r3col2, r3col3 = st.columns([0.25, 4, 0.25])
+
+
+
+with r1col2:
+  st.title("Date Picker Test")
+with r2col2:
+  st.divider()
+with r3col2:
+  selected_date = st.date_input("Pick a date", datetime.date.today())
+  st.write("You selected:", selected_date)
 
