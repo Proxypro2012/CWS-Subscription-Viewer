2c7dc7f6baad62be813df931a8a521058a89654f
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-05-18 00:18:30 -0400

    Update main.py

--- front-end/test/authtest/main.py
+++ front-end/test/authtest/main.py
@@ -11,7 +11,7 @@ st.set_page_config(
 
 
 
-r1col1, r1col2, r1col3 = st.columns([1, 6, 1])
+r1col1, r1col2, r1col3 = st.columns([1, 3, 1])
 r2col1, r2col2, r2col3 = st.columns([0.25, 4, 0.25])
 r3col1, r3col2, r3col3 = st.columns([0.25, 4, 0.25])
 r4col1, r4col2, r4col3 = st.columns([0.25, 4, 0.25])
