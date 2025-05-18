8c07d3e444c7a5e28d281355b9bee39912299713
Proxypro2012 <kabir.tiwari@outlook.com>
2025-05-18 12:05:16 -0400

    FIX NEW 14 4

--- front-end/test/authtest/main.py
+++ front-end/test/authtest/main.py
@@ -7,43 +7,70 @@ st.set_page_config(page_title="CWS Subscription Viewer")
 
 BASE_URL = "https://cws-subscription-viewer.onrender.com"
 
-# UI Layout
-r1col1, r1col2, r1col3 = st.columns([1, 0.5, 1])
-r2col1, r2col2, r2col3 = st.columns([0.25, 4, 0.25])
-r3col1, r3col2, r3col3 = st.columns([0.25, 4, 0.25])
-r4col1, r4col2, r4col3 = st.columns([0.25, 4, 0.25])
 
-# Initialize login state
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
 
-with r1col2:
-    st.title("Login")
 
-with r2col2:
-    username = st.text_input("Username", placeholder="Enter your username")
-with r3col2:
-    password = st.text_input("Password", placeholder="Enter your password", type="password")
 
-with r4col2:
-    if st.button("Login", type="primary"):
-        creds = {"username": username, "password": password}
+def login():
+    # UI Layout
+    r1col1, r1col2, r1col3 = st.columns([1, 0.5, 1])
+    r2col1, r2col2, r2col3 = st.columns([0.25, 4, 0.25])
+    r3col1, r3col2, r3col3 = st.columns([0.25, 4, 0.25])
+    r4col1, r4col2, r4col3 = st.columns([0.25, 4, 0.25])
+
+    # Initialize login state
+    if "logged_in" not in st.session_state:
+        st.session_state.logged_in = False
+
+    with r1col2:
+        st.title("Login")
+
+    with r2col2:
+        username = st.text_input("Username", placeholder="Enter your username")
+    with r3col2:
+        password = st.text_input("Password", placeholder="Enter your password", type="password")
+
+    with r4col2:
+        if st.button("Login", type="primary"):
+            creds = {"username": username, "password": password}
+            
+            try:
+                response = requests.post(f"{BASE_URL}/login", json=creds)
+                # st.write(f"Response status: {response.status_code}")
+                # st.write(response.text)  # Show full response for debugging
+
+                if response.status_code == 200:
+                    st.success("Login successful!")
+                    st.session_state.logged_in = True
+                    st.session_state.username = username
+                    st.session_state.password = password
+                else:
+                    st.error("Login failed. Please check your credentials.")
+            except Exception as e:
+                st.error(f"Request failed: {e}")
+
+
+    # Trigger the rain
+    rain('•', 20, falling_speed=5, animation_length="infinite")
+
+
+def dashboard():
+    r1col1, r1col2, r1col3 = st.columns([1, 0.5, 1])
+    r2col1, r2col2, r2col3 = st.columns([0.25, 4, 0.25])
+    r3col1, r3col2, r3col3 = st.columns([0.25, 4, 0.25])
+    r4col1, r4col2, r4col3 = st.columns([0.25, 4, 0.25])
+
+    with r1col2:
+        st.title("Dashboard")
+    with r2col2:
+        st.divider()
         
-        try:
-            response = requests.post(f"{BASE_URL}/login", json=creds)
-            # st.write(f"Response status: {response.status_code}")
-            # st.write(response.text)  # Show full response for debugging
-
-            if response.status_code == 200:
-                st.success("Login successful!")
-                st.session_state.logged_in = True
-                st.session_state.username = username
-                st.session_state.password = password
-            else:
-                st.error("Login failed. Please check your credentials.")
-        except Exception as e:
-            st.error(f"Request failed: {e}")
-
-
-# Trigger the rain
-rain('•', 20, falling_speed=5, animation_length="infinite")
+        
+        
+        
+if not st.session_state.logged_in:
+    # Show login page if not logged in
+    login()
+else:
+    # Show dashboard page if logged in
+    dashboard()
