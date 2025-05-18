3d9679dcac561611b01526071880372ed999f821
Proxypro2012 <kabir.tiwari@outlook.com>
2025-05-18 16:44:03 -0400

    NEW 18

--- front-end/test/authtest/main.py
+++ front-end/test/authtest/main.py
@@ -70,7 +70,26 @@ def dashboard():
         
 
 
+def homepage():
+    r1col1, r1col2, r1col3 = st.columns([0.25, 4, 0.25])
+    r2col1, r2col2, r2col3 = st.columns([0.25, 4, 0.25])
+    r3col1, r3col2, r3col3 = st.columns([0.25, 4, 0.25])
+    r4col1, r4col2, r4col3 = st.columns([0.25, 4, 0.25])
 
+    with r1col2:
+        st.title("Home Page")
+    with r2col2:
+        st.divider()
+        st.write("Welcome to the SBM Website!")
+    with r3col2:
+        if st.button("Login", type="primary"):
+            st.session_state.logged_in = False
+            st.session_state.username = ""
+            st.session_state.password = ""
+            st.experimental_rerun()
+    with r4col2:
+        if st.button("Sign Up", type="primary"):
+            pass
         
 if not st.session_state.logged_in:
     # Show login page if not logged in
