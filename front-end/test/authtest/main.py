7af0e3aa0e006cc807c47ae4bcb9adfdb9761c62
Proxypro2012 <kabir.tiwari@outlook.com>
2025-05-19 23:18:05 -0400

    NEW 62 FIX

--- front-end/test/authtest/main.py
+++ front-end/test/authtest/main.py
@@ -141,7 +141,7 @@ def dashboard():
 
     
     with st.sidebar:
-        st.page_link("home.py", label="Home", icon="🏠")
+        st.page_link("main.py", label="Home", icon="🏠")
         st.page_link("pages/Settings.py", label="Settings", icon="⚙️")  
 
 
