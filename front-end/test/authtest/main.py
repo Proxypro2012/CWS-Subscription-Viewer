b3a1a263f4794913d8ab6a1f50fa78ee128f0871
Proxypro2012 <kabir.tiwari@outlook.com>
2025-05-19 22:55:19 -0400

    NEW 57

--- front-end/test/authtest/main.py
+++ front-end/test/authtest/main.py
@@ -164,7 +164,7 @@ def dashboard():
             "container": {
                 "padding": "0!important",
                 "background-color": "#ffffff",
-                "width": "1000%",
+                "width": "100%",
                 "justify-content": "center"
             },
             "icon": {"color": "#000", "font-size": "18px"},
