d7994d9f7496521b57884b1927b7e612676fb6de
Proxypro2012 <kabir.tiwari@outlook.com>
2025-05-20 18:44:41 -0400

    NEW 82

--- front-end/test/authtest/main.py
+++ front-end/test/authtest/main.py
@@ -162,28 +162,32 @@ def settings():
         if st.button("Settings"):
             st.session_state.page = "settings"
             st.rerun()
+
+
+        @st.dialog("Confirm Logout")
+        def confirm_logout():
+            st.write("Are you sure you want to log out?")
+            col1, col2 = st.columns(2)
+            with col1:
+                if st.button("Yes, log out"):
+                    st.session_state.logged_in = False
+                    st.session_state.page = "home"
+                    st.session_state.username = ""
+                    st.session_state.password = ""
+                    st.rerun()
+            with col2:
+                if st.button("Cancel"):
+                    st.toast("Logout cancelled.")
+
+       
         if st.button("Logout"):
-            st.session_state.logged_in = False
-            st.session_state.page = "home"
-            st.session_state.username = ""
-            st.session_state.password = ""
-            st.rerun()
+            confirm_logout()
+
+
+
 
     st.title("Settings")
     st.info("Settings page content goes here.")
-
-
-        
-
-
-
-        
-       
-
-
-
-
-
 
 
 
